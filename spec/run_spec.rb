75bdbf7b8c3eed393801256a0d9ef87ff7a9a33f
Daniel Vartanov <dan@vartanov.net>
2020-07-01 08:40:26 +0100

    Split up tests into multiple spec files

---
+++ spec/run_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "tty/testing"
+
+RSpec.describe TTY::Testing::App, '#run!' do
+  let(:app) do
+    TTY::Testing.app_wrapper do |_, output|
+      output.puts 'Hello, world!'
+    end
+  end
+
+  context 'before `#run!` is called' do
+    it 'does not execute the application' do
+      expect(app.output).to be_empty
+    end
+
+    context 'when `#run!` is called' do
+      before { app.run! }
+
+      it 'executes the application' do
+        expect(app.output).to eq "Hello, world!\n"
+      end
+    end
+  end
+end
