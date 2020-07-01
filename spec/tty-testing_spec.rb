183518bc421dbff99c548ad5d334a36bb012f2e4
Daniel Vartanov <dan@vartanov.net>
2020-07-01 08:34:23 +0100

    Add specs for `App#run!`

--- spec/tty-testing_spec.rb
+++ spec/tty-testing_spec.rb
@@ -115,4 +115,26 @@ RSpec.describe TTY::Testing::App do
       end
     end
   end
+
+  describe '#run!' do
+    let(:app) do
+      TTY::Testing.app_wrapper do |_, output|
+        output.puts 'Hello, world!'
+      end
+    end
+
+    context 'before `#run!` is called' do
+      it 'does not execute the application' do
+        expect(app.output).to be_empty
+      end
+
+      context 'when `#run!` is called' do
+        before { app.run! }
+
+        it 'executes the application' do
+          expect(app.output).to eq "Hello, world!\n"
+        end
+      end
+    end
+  end
 end
